0a32298157b46d49a493aae64a1c23242c1eb1c1
Matt Lally <mattlally@macbook-pro-2.mynetworksettings.com>
2025-12-25 17:45:07 -0500

    Fix base path to /amazon_wrapped/ for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 // For local dev: set base to "/"
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/amazon-wrapped-web/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/amazon_wrapped/' : '/',
   optimizeDeps: {
     include: ['plotly.js', 'react-plotly.js', 'buffer'],
   },
